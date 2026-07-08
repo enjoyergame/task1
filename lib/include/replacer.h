cf20dc0314afe08248a019f3b8da0ea10119dd4c
Кучуков Ридаль <newnewfrfr@proton.me>
2026-07-09 02:01:01 +0500

    replace str to byte
    add func strtobyte and free_replacer

--- lib/include/replacer.h
+++ lib/include/replacer.h
@@ -1,6 +1,26 @@
 /*
-Только функции запуска замены
+Только функции замены
 
 Кучуков Ридаль Радикович
 МК-101
 */
+
+#ifndef REPLACER_H
+#define REPLACER_H
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+typedef struct
+{
+    uint8_t *data;
+    size_t length;
+    size_t match_idx;
+} replacer;
+
+replacer strtobyte(char *input_str);
+void free_replacer(replacer *r);
+
+#endif
--- lib/src/replacer.c
+++ lib/src/replacer.c
@@ -4,3 +4,31 @@
 Кучуков Ридаль Радикович
 МК-101
 */
+
+#include "replacer.h"
+
+replacer strtobyte(char* input_str) {
+    replacer r = {NULL, 0, 0};
+    if(input_str == NULL) return r;
+    size_t str_len = strlen(input_str);
+    r.length = str_len / 2;
+    r.data = (uint8_t*)malloc(r.length);
+    if (r.data == NULL) {
+    r.length = 0;
+    return r;     
+}
+    for (size_t i = 0; i < (r.length); i++){
+            char buf[3] = {input_str[i * 2], input_str[i * 2 + 1], '\0'};
+            r.data[i] = (uint8_t)strtol(buf, NULL, 16);
+    }
+    return r;
+}
+
+void free_replacer(replacer* r) {
+    if (r == NULL) return;
+
+    free(r->data);
+    r->data = NULL;
+    r->length = 0;
+    r->match_idx = 0;
+}
